7d81ddf514b9ece18bc3522504010ff63daa44bb
6one2 <sebastien.villard@gmail.com>
2020-11-15 21:45:53 -0500

    app in top level directory

--- model/train_classifier.py
+++ model/train_classifier.py
@@ -106,6 +106,7 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
+    
     with open(model_filepath+'.z','wb') as f: 
         joblib.dump(model.best_estimator_, f, compress=True)
 
